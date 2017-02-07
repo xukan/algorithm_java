90d2145e8efef2afdabb40ae40c6593ef5ff1a46
xukan <xukan111@gmail.com>
2017-02-07 17:25:46 -0500

    remove k digits

--- RemoveKDigits.java
+++ RemoveKDigits.java
@@ -2,34 +2,56 @@ package algorithm_java;
 //Google Snapchat
 
 public class RemoveKDigits {
+//	public String removeKdigits(String num, int k) {
+//	    if(num.length()==k)
+//	        return "0";
+//	 
+//	    StringBuilder sb = new StringBuilder(num);
+//	    for(int j=0; j<k; j++){
+//	        int i=0;
+//	        while(i<sb.length()-1&&sb.charAt(i)<=sb.charAt(i+1)){
+//	            i++;
+//	        }
+//	        sb.delete(i, i+1);
+//	    }
+//	 
+//	    //remove leading 0's        
+//	    while (sb.length() > 1 && sb.charAt(0)=='0')
+//	        sb.delete(0,1);
+//	 
+//	    if(sb.length()==0){
+//	        return "0";
+//	    }
+//	 
+//	    return sb.toString();
+//	}
+	
 	public String removeKdigits(String num, int k) {
-	    if(num.length()==k)
-	        return "0";
-	 
-	    StringBuilder sb = new StringBuilder(num);
-	    for(int j=0; j<k; j++){
-	        int i=0;
-	        while(i<sb.length()-1&&sb.charAt(i)<=sb.charAt(i+1)){
-	            i++;
-	        }
-	        sb.delete(i, i+1);
-	    }
-	 
-	    //remove leading 0's        
-	    while (sb.length() > 1 && sb.charAt(0)=='0')
-	        sb.delete(0,1);
-	 
-	    if(sb.length()==0){
-	        return "0";
-	    }
-	 
-	    return sb.toString();
-	}
+        if(k>=num.length())
+            return "0";
+        StringBuilder sb = new StringBuilder(num);
+        
+        for(int i=0;i<k;i++){
+        	int j=0;
+            while(j<num.length()-1 && sb.charAt(j)<=sb.charAt(j+1)){
+                j++;
+            }
+            sb.deleteCharAt(j);
+        }
+        
+        //if(num.length()==k)
+        while(sb.length()>1 && sb.charAt(0)=='0')
+            sb.deleteCharAt(0);
+        if(sb.length()==0)
+            return "0";
+        return sb.toString();
+    }
 	
 	public static void main(String[] args) {
 		String input = "1432219";
+		String input1 = "1111111";
 		RemoveKDigits s = new RemoveKDigits();
-		String res = s.removeKdigits(input, 3);
+		String res = s.removeKdigits(input1, 3);
 		System.out.println(res);
 	}
 }
--- Test.java
+++ Test.java
@@ -22,93 +22,47 @@ class TreeNode {
 //}
 
 public class Test {
-	/** Initialize your data structure here. */
-    class Tweet{
-        int time;
-        int tid;
-        Tweet(int t, int tweetid){
-            time=t;
-            tid = tweetid;
+	public ListNode reverseBetween(ListNode head, int m, int n) {
+        if(head ==null || head.next ==null )
+            return head;
+        ListNode dummy=new ListNode(-1);
+        dummy.next = head;
+        ListNode node = head;
+        ListNode slow = dummy, fast = dummy;
+        for(int i=1;i<m;i++)
+            slow = slow.next;
+        for(int i=0;i<n;i++) 
+            fast = fast.next;
+        /*             1      2      3     4     5
+         * dumy ->1 ->  2 -> 3 -> 4 -> 5
+         *             s      tail           f
+         * */
+        ListNode tail = slow.next;
+        slow.next = fast;
+        slow = tail;
+        tail = tail.next;
+        slow.next= fast.next;
+        for(int i=m;i<n;i++){
+            ListNode next = tail.next;
+            tail.next = slow;
+            slow =tail;
+            tail = next;
         }
+        return dummy.next;
     }
-    int timestamp;
-    Map<Integer, List<Tweet>> timelines;
-    Map<Integer, HashSet<Integer>> relations;
-    public Test() {
-        timestamp =0;
-        timelines = new HashMap<Integer, List<Tweet>>();
-        relations = new HashMap<Integer, HashSet<Integer>>();
-    }
-    
-    /** Compose a new tweet. */
-    public void postTweet(int userId, int tweetId) {
-        if(!timelines.containsKey(userId)){
-            timelines.put(userId, new ArrayList<Tweet>());
-        }
-        timelines.get(userId).add(new Tweet(timestamp++, tweetId));
-    }
-    
-    /** Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent. */
-    public List<Integer> getNewsFeed(int userId) {
-        List<Tweet> tweets = timelines.get(userId);
-        List<Tweet> candidates = new ArrayList<Tweet>();
-        for(int i=tweets.size()-1; i >= Math.max(0, tweets.size()-10) ;i--)
-            candidates.add(tweets.get(i));
-        HashSet<Integer> followees = relations.get(userId);
-        if(followees!=null){
-            for(int followee: followees){
-                List<Tweet> ftweets = timelines.get(followee);
-                for(int i=ftweets.size()-1; i >= Math.max(0, ftweets.size()-10) ;i--)
-                    candidates.add(ftweets.get(i));
-            }
-        }
-        Collections.sort(candidates, (Tweet t1, Tweet t2) -> (t2.time - t1.time));
-        List<Integer> res = new ArrayList<Integer>();
-        for(int i=0;i<Math.min(10, candidates.size());i++)
-            res.add(candidates.get(i).tid);
-        return res;
-    }
-    
-    /** Follower follows a followee. If the operation is invalid, it should be a no-op. */
-    public void follow(int followerId, int followeeId) {
-        if(followerId == followeeId)
-            return;
-        if(!relations.containsKey(followerId))
-            relations.put(followerId, new HashSet<Integer>());
-        relations.get(followerId).add(followeeId);
-    }
-    
-    /** Follower unfollows a followee. If the operation is invalid, it should be a no-op. */
-    public void unfollow(int followerId, int followeeId) {
-        HashSet<Integer> set = relations.get(followerId);
-        if(set == null)
-            return;
-        set.remove(followeeId);
-    }
-    
-    public static void main(String[] args) {
-		Test twitter = new Test();
-		List<Integer> res  = new ArrayList();
-		// User 1 posts a new tweet (id = 5).
-		twitter.postTweet(1, 1);
-
-		// User 1's news feed should return a list with 1 tweet id -> [5].
-		twitter.getNewsFeed(1);
-
-		// User 1 follows user 2.
-		twitter.follow(2, 1);
-
-		// User 2 posts a new tweet (id = 6).
-		res =twitter.getNewsFeed(2);
-		for(int i: res)
-			System.out.print( i + " " );
-		System.out.println();
-		
-		twitter.unfollow(2,1);
-		
-		// User 1's news feed should return a list with 2 tweet ids -> [6, 5].
-		// Tweet id 6 should precede tweet id 5 because it is posted after tweet id 5.
-		twitter.getNewsFeed(2);
-
+	
+	public static void main(String[] args){
+		ListNode head = new ListNode(1);
+		head.next = new ListNode(2);
+		head.next.next = new ListNode(3);
+		head.next.next.next = new ListNode(4);
+		head.next.next.next.next = new ListNode(5);
+		//head = reverseList(head);
+		Test s = new Test();
+		head = s.reverseBetween(head, 2,4);
+		while(head!=null){
+			System.out.println(head.val);
+			head = head.next;
+		}
 	}
 }
